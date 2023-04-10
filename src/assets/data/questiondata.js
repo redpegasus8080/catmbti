e82724d12c6e378c52f26db208c5b011cc396021
redpegasus8080 <108916626+redpegasus8080@users.noreply.github.com>
2023-04-10 10:48:12 +0900

    add question

---
+++ src/assets/data/questiondata.js
@@ -0,0 +1,14 @@
+export const QuestionData = [
+    {id: 1, title: "문제1", answera: "문제1 - 답변1", answerb: "문제1 - 답변2", type: "EI"},
+    {id: 2, title: "문제2", answera: "문제2 - 답변1", answerb: "문제2 - 답변2", type: "EI"},
+    {id: 3, title: "문제3", answera: "문제3 - 답변1", answerb: "문제3 - 답변2", type: "EI"},
+    {id: 4, title: "문제4", answera: "문제4 - 답변1", answerb: "문제4 - 답변2", type: "EI"},
+    {id: 5, title: "문제5", answera: "문제5 - 답변1", answerb: "문제5 - 답변2", type: "EI"},
+    {id: 6, title: "문제6", answera: "문제6 - 답변1", answerb: "문제6 - 답변2", type: "EI"},
+    {id: 7, title: "문제7", answera: "문제7 - 답변1", answerb: "문제7 - 답변2", type: "EI"},
+    {id: 8, title: "문제8", answera: "문제8 - 답변1", answerb: "문제8 - 답변2", type: "EI"},
+    {id: 9, title: "문제9", answera: "문제9 - 답변1", answerb: "문제9 - 답변2", type: "EI"},
+    {id: 10, title: "문제10", answera: "문제10 - 답변1", answerb: "문제10 - 답변2", type: "EI"},
+    {id: 11, title: "문제11", answera: "문제11 - 답변1", answerb: "문제11 - 답변2", type: "EI"},
+    {id: 12, title: "문제12", answera: "문제12 - 답변1", answerb: "문제12 - 답변2", type: "EI"}    
+]
