23ee6ceb6db316023e5d153a3739a67d1a8a347f
redpegasus8080 <108916626+redpegasus8080@users.noreply.github.com>
2023-04-10 13:19:48 +0900

    question logic

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,23 +1,98 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ProgressBar, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { QuestionData } from '../assets/data/questiondata';
 
+
 const Question = () => {
+    const [questionNo, setQuestionNo] = React.useState(0);
+    const [totalScore, setTotalScore] = React.useState([
+        { id: "EI", score: 0 },
+        { id: "SN", score: 0 },
+        { id: "TF", score: 0 },
+        { id: "JP", score: 0 },
+    ]);
+    const navigate = useNavigate();
+
+    console.log('totalScore', totalScore);
+
+    const hancleClickButton = (no, type) => {
+        const newScore = totalScore.map((s) => 
+            s.id === type ? { id: s.id, score: s.score + no } : s
+        );
+
+        setTotalScore(newScore);
+        // 다음문제로 문제수 증가
+        if(QuestionData.length !== questionNo + 1) {
+            setQuestionNo(questionNo + 1);
+        }else{
+            // 결과 페이지 이동
+            navigate("/result");
+        }
+
+        // if ( type === "EI" ) {
+        //     // 기존 스코어에 더할 값을 계산 (기존의 값 + 배점)
+        //     const addScore = totalScore[0].score + no;
+        //     // 새로운 객체
+        //     const newObject = { id: type, score: addScore };
+        //     // splice 통해 새로운 객체를 해당 객체 자리에 넣어줌
+        //     totalScore.splice(0, 1,  newObject);
+
+        // }else if ( type === "SN" ) {
+        //     const addScore = totalScore[0].score + no;
+        //     const newObject = { id: type, score: addScore };
+        //     totalScore.splice(1, 1,  newObject);
+
+        // }else if ( type === "TF" ) {
+        //     const addScore = totalScore[0].score + no;
+        //     const newObject = { id: type, score: addScore };
+        //     totalScore.splice(2, 1,  newObject);
+
+        // }else if ( type === "JP" ) {
+        //     const addScore = totalScore[0].score + no;
+        //     const newObject = { id: type, score: addScore };
+        //     totalScore.splice(3, 1,  newObject);
+        // }
+
+        // setQuestionNo(questionNo + 1);
+    }
+
+    // const hancleClickButtonB = (no, type) => {
+    //     if ( type === "EI" ) {
+    //         // 기존 스코어에 더할 값을 계산 (기존의 값 + 배점)
+    //         const addScore = totalScore[0].score + no;
+    //         // 새로운 객체
+    //         const newObject = { id: type, score: addScore };
+    //         // splice 통해 새로운 객체를 해당 객체 자리에 넣어줌
+    //         totalScore.splice(0, 1,  newObject);
+
+    //     }else if ( type === "SN" ) {
+    //         const addScore = totalScore[0].score + no;
+    //         const newObject = { id: type, score: addScore };
+    //         totalScore.splice(1, 1,  newObject);
+
+    //     }else if ( type === "TF" ) {
+    //         const addScore = totalScore[0].score + no;
+    //         const newObject = { id: type, score: addScore };
+    //         totalScore.splice(2, 1,  newObject);
+
+    //     }else if ( type === "JP" ) {
+    //         const addScore = totalScore[0].score + no;
+    //         const newObject = { id: type, score: addScore };
+    //         totalScore.splice(3, 1,  newObject);
+    //     }
+
+    //     setQuestionNo(questionNo + 1);
+    // }
 
     return (
         <Wrapper>
-            <ProgressBar striped variant="danger" now={80} style={{ marginTop: '20px' }}/>
-            <Title>{QuestionData[0].title}</Title>
+            <ProgressBar striped variant="danger" now={(questionNo / QuestionData.length) * 100} style={{ marginTop: '20px' }}/>
+            <Title>{QuestionData[questionNo].title}</Title>
             <ButtonGroup>
-                <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>{QuestionData[0].answera}</Button>
-                <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }}>{QuestionData[0].answerb}</Button>
-            </ButtonGroup>
-
-            <Title>{QuestionData[1].title}</Title>
-            <ButtonGroup>
-                <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>{QuestionData[1].answera}</Button>
-                <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }}>{QuestionData[1].answerb}</Button>
+                <Button onClick={() => hancleClickButton(1, QuestionData[questionNo].type)} style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>{QuestionData[questionNo].answera}</Button>
+                <Button onClick={() => hancleClickButton(0, QuestionData[questionNo].type)} style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }}>{QuestionData[questionNo].answerb}</Button>
             </ButtonGroup>
         </Wrapper>
     )
