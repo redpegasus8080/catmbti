b3338eca71d3e4ff8f42fb2167e91ef22ecb360f
redpegasus8080 <108916626+redpegasus8080@users.noreply.github.com>
2023-04-10 17:17:05 +0900

    add result logic

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ProgressBar, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 import { QuestionData } from '../assets/data/questiondata';
 
 const Question = () => {
@@ -26,8 +26,22 @@ const Question = () => {
         if(QuestionData.length !== questionNo + 1) {
             setQuestionNo(questionNo + 1);
         }else{
+            // 결과 도출
+            const mbti = newScore.reduce(
+                (acc, curr) => 
+                    acc + (curr.score >= 2 ? curr.id.substring(0,1) : curr.id.substring(1,2)), 
+                ""
+            );
+
+            console.log(mbti);
             // 결과 페이지 이동
-            navigate("/result");
+            // navigate("/result");
+            navigate({
+                pathname: "/result",
+                search: `?${createSearchParams({
+                    mbti: mbti,
+                })}`
+            });
         }
 
         // if ( type === "EI" ) {
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,24 +1,34 @@
 import React from 'react';
 // css-in-js
 import styled from 'styled-components';
-import PangImage from '../assets/darkSky.jpg';
+// import PangImage from '../assets/darkSky.jpg';
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
-import { QuestionData } from '../assets/data/questiondata';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { ResultData } from '../assets/data/resultdata';
 
 const Result = () => {
     const navigate = useNavigate();
-    
+    const [searchParams] = useSearchParams();
+    const mbti = searchParams.get('mbti');
+    // 최종 결과 데이터
+    const [resultData, setResultData] = React.useState({});
+
+    React.useEffect(() => {
+        const result = ResultData.find((s) => s.best === mbti);
+        setResultData(result);
+    }, [mbti]);
+
+    console.log(resultData);
+
     return (
         <Wrapper>
             <Header>PEGASUS 천마</Header>
             <Contents>
                 <Title>결과 보기</Title>
                 <LogoImage>
-                    <img src={ResultData[0].image} alt="" className="rounded-circle" width={350} height={350} />
+                    <img src={resultData.image} alt="" className="rounded-circle" width={350} height={350} />
                 </LogoImage>
-                <Desc>예비 결과 설명입니다. {ResultData[0].name}  </Desc>
+                <Desc>예비 결과 설명입니다. {resultData.best} / {resultData.name}  </Desc>
                 <Button style={{ fontFamily: "SimKyungha"}} onClick={() => navigate("/")}>
                     테스트 다시하기
                 </Button>
